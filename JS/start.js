f95f1c31dc1cef750718dcc5d1863f2a3e6660ff
kevkevinpal <oapallikunnel@gmail.com>
2016-10-11 13:35:36 -0500

    Little fix for me with chrome dev popup

--- JS/start.js
+++ JS/start.js
@@ -3,3 +3,10 @@ link.href =  chrome.extension.getURL('darkpandora.css');
 link.rel = 'stylesheet';
 head = document.getElementsByTagName ("head")[0] || document.documentElement;
 head.insertBefore(link, head.firstChild);
+
+
+//This will disable the chrome popup for me dont mind :)
+ChromeOptions options = new ChromeOptions();
+options.addArguments("chrome.switches","--disable-extensions");
+System.setProperty("webdriver.chrome.driver",(System.getProperty("user.dir") + "//src//test//resources//chromedriver_new.exe"));
+driver = new ChromeDriver(options);
